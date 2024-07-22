6e7d2de0bcaa2e66bed66c158a5c871007315540
Maximo Olivera <maximo.a.olivera@gmail.com>
2024-07-22 11:14:31 -0300

    fix arrows

--- lua/mazzt/remap.lua
+++ lua/mazzt/remap.lua
@@ -5,10 +5,10 @@ vim.keymap.set("n", "<leader>fmd", vim.lsp.buf.format)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
-vim.keymap.set("n", "<left>", ":bn<cr>")
-vim.keymap.set("n", "<right>", ":bp<cr>")
+vim.keymap.set("n", "<right>", ":bn<cr>")
+vim.keymap.set("n", "<left>", ":bp<cr>")
 vim.keymap.set("n", "<leader>bx", ":bd<cr>")
 
 -- better way to jump to start or end of line
-vim.keymap.set('', 'H', '^')
+vim.keymap.set('', 'H', '_')
 vim.keymap.set('', 'L', '$')
